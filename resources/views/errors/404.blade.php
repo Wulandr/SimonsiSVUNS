01b8f877f3f788c70bb1cb5a30e3abac22adbe07
Wulandr <wdari0161@gmail.com>
2022-06-14 16:09:21 +0700

    eh tadi salah

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -16,29 +16,8 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
-    <link href="{{ asset('assets/img/logo.png') }}" rel="icon">
-    <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('findash/assets/css/bootstrap.min.css') }}">
-    <!-- Typography CSS -->
-    <link rel="stylesheet" href="{{ asset('findash/assets/css/typography.css') }}">
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="{{ asset('findash/assets/css/style.css') }}">
-    <!-- Responsive CSS -->
-    <link rel="stylesheet" href="{{ asset('findash/assets/css/responsive.css') }}">
-    <!-- Full calendar -->
-    <link href='{{ asset('findash/assets/fullcalendar/core/main.css') }}' rel='stylesheet' />
-    <link href='{{ asset('findash/assets/fullcalendar/daygrid/main.css') }}' rel='stylesheet' />
-    <link href='{{ asset('findash/assets/fullcalendar/timegrid/main.css') }}' rel='stylesheet' />
-    <link href='{{ asset('findash/assets/fullcalendar/list/main.css') }}' rel='stylesheet' />
-    <link rel="stylesheet" href="{{ asset('findash/assets/css/flatpickr.min.css') }}">
-    <link rel="" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
-    <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+
+
 </head>
 
 <body class="iq-bg-primary">
